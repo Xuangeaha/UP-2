d45e6e8f3858ec6ef4e465fd77f6d3dff1e8fb6b
Xuangeaha <xiangzhenxuan@163.com>
2023-03-05 15:58:23 +0800

    1.0.0

---
+++ array.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+
+namespace UP
+{
+    class UP
+    {
+        private static void printt(params string[] args)
+        {
+            Console
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("我要当up主！2023.0.1");
+            Console.WriteLine("--------------------------------------------------");
+
+            bool processing = false;
+
+            int day = 0;
+            int hour_tick = 8;
+            int minute_tick = 0;
+            string hour_display = "08";
+            string minute_display = "00";
+
+            int num = 0;
+
+            int money = 100;
+
+            int follower = 0;
+            int play_all = 0;
+
+            int energy = 100;
+            int hunger = 100;
+
+            ArrayList video = new ArrayList();
+            ArrayList video_spread = new ArrayList();
+            ArrayList video_play = new ArrayList();
+            ArrayList video_release_time = new ArrayList();
+
+            int advice1 = 0;
+
+            ArrayList tags = new ArrayList() {"我的世界","游戏","iPhone 14","疫情","科技","猫和老鼠","何同学","籽岷","布洛芬","网课","蔡徐坤","原神","Minecraft","世界杯",
+                    "阿根廷","鬼畜","社交","知识","纪录片","核酸","抗原","华强","人类高质量","俄乌局势","PlayStation","XBox","苹果","微软","华为",
+                    "二十大","2023","英伟达","4090","4090 ti","腾讯","体育","阳康"};
+        }
+    }
+}
